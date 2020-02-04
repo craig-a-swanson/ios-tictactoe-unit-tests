5e7fbd35dbcf52a035fa06389400a5fb10b7d7ac
Craig Swanson <cswanson02@me.com>
2020-02-04 12:44:45 -0600

    Added one more scenario to testing if the game is incomplete

--- TicTacToeTests/GameAITests.swift
+++ TicTacToeTests/GameAITests.swift
@@ -142,14 +142,13 @@ class GameAITests: XCTestCase {
          XCTAssertFalse(game(board: board, isWonBy: .x))
     }
     
-    func testIncompleteGame() {
+    func testIncompleteGame1() {
         // check to see if there is a winner
             // if there is a winner, then return false (the game is complete)
         // check to see if the game board is full
             // if the board is full, then return false (the game is complete)
         // if neither is true, then return true (the game is incomplete)
         
-        // Or, just test the isFull property to make sure it works.
         var board = GameBoard()
         try! board.place(mark: .x, on: (0, 0))
         try! board.place(mark: .o, on: (0, 1))
@@ -157,11 +156,44 @@ class GameAITests: XCTestCase {
         try! board.place(mark: .o, on: (1, 1))
         try! board.place(mark: .x, on: (1, 2))
         try! board.place(mark: .o, on: (2, 0))
+        XCTAssertFalse(game(board: board, isWonBy: .o))
+        XCTAssertFalse(game(board: board, isWonBy: .x))
         XCTAssertFalse(board.isFull)
         
         try! board.place(mark: .x, on: (0, 2))
         try! board.place(mark: .o, on: (2, 2))
         try! board.place(mark: .x, on: (2, 1))
+
+        XCTAssertFalse(game(board: board, isWonBy: .o))
+        XCTAssertFalse(game(board: board, isWonBy: .x))
+        XCTAssertTrue(board.isFull)
+        
+    }
+    
+    func testIncompleteGame2() {
+        // player x has a winning scenario with the last move.
+        /*
+         x x x
+         o o x
+         o x o
+         */
+        var board = GameBoard()
+        try! board.place(mark: .x, on: (0, 0))
+        try! board.place(mark: .o, on: (0, 1))
+        try! board.place(mark: .x, on: (1, 0))
+        try! board.place(mark: .o, on: (1, 1))
+        try! board.place(mark: .x, on: (2, 1))
+        try! board.place(mark: .o, on: (0, 2))
+        XCTAssertFalse(game(board: board, isWonBy: .o))
+        XCTAssertFalse(game(board: board, isWonBy: .x))
+        XCTAssertFalse(board.isFull)
+        
+        try! board.place(mark: .x, on: (1, 2))
+        try! board.place(mark: .o, on: (2, 2))
+        try! board.place(mark: .x, on: (2, 0))
+
+        XCTAssertFalse(game(board: board, isWonBy: .o))
+        XCTAssertTrue(game(board: board, isWonBy: .x))
         XCTAssertTrue(board.isFull)
         
     }
