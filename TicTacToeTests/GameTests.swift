41af87167f71ed4effd7193a19c4125f210dca4d
Craig Swanson <cswanson02@me.com>
2020-02-04 21:42:55 -0600

    Created GameTests file for UITests.

---
+++ TicTacToeTests/GameTests.swift
@@ -0,0 +1,17 @@
+//
+//  GameTests.swift
+//  TicTacToeTests
+//
+//  Created by Craig Swanson on 2/4/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import XCTest
+@testable import TicTacToe
+
+class GameTests: XCTestCase {
+
+    
+
+
+}
